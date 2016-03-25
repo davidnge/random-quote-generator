2c75eb8af2e847301fd482e9ec19682c86da79ce
nge chee keen <david.nnck@gmail.com>
2016-03-25 10:59:59 +0800

    added project random quote generator

---
+++ app.js
@@ -0,0 +1,41 @@
+var quoteJson;
+var newQuote = '';
+
+function quoteJson() { $.ajax({
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/', 
+        type: 'POST', 
+        data: {}, 
+        datatype: 'json',
+        success: function(data) { var l = JSON.parse(data);
+                                    newQuote = l.quote;
+                                    newAuthor = l.author;
+                                    console.log(newAuthor);
+                                    $('#quote').animate({opacity:0}, 500, function(){
+                                        $(this).animate({opacity:1}, 500);
+                                        $('#quote').html('<span class="quote-mark">“</span><div class="quote-text">'+newQuote+'</div>');
+                                        $('#author').html('<div class="quote-author"> — '+newAuthor+'</div>');
+                                    });
+                                    
+                                    $('.tweet-quote').attr('href','https://www.twitter.com/intent/tweet?hashtag=quotes&text='+encodeURIComponent(newQuote));
+                                },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "q0W1uYyYXimshGkoYUZylOxPCjwrp1SYE1cjsnRkQYdQjS8nM4"); 
+        }
+    });
+    
+    $(document).ready(function(){
+        $('#tweet-quote').on('click', function(){
+            
+            $('#tweet-quote').attr('href', '')     
+        });
+    });
+
+}
+
+$(document).ready(function(){
+  quoteJson();
+   $('#new-quote').on('click', quoteJson)
+});
+
+
